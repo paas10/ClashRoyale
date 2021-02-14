026ad1b7ddf4060c0a768e8a9cd8632382c6e328
Pablo Alvarado <pablo.andre_alvarado@hotmail.com>
2021-02-13 20:19:45 -0600

    Implementación de Redirect Route

--- clashroyale/src/index.js
+++ clashroyale/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import Login from './components/Login';
+import ResetPassword from './components/ResetPassword';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import 'fontsource-roboto';
