e69e958e6a4b16fbdef50a224fc9ee67ae1b5340
Pablo Alvarado <pablo.andre_alvarado@hotmail.com>
2021-02-16 22:51:58 -0600

    Avances con la página Home

--- clashroyale/src/components/Home.js
+++ clashroyale/src/components/Home.js
@@ -1,15 +1,59 @@
 import { React, Component } from "react";
 import Layout from "./Layout";
-// import "./styles/Home.css";
+import "./styles/Home.css";
+import { IoMdAdd } from "react-icons/io";
 
 class Home extends Component {
   render() {
     return (
       <Layout>
-        hola
+        {/* ENCABEZADO DE LA VISTA */}
+        <div className="row">
+          <div className="col-10">
+            <span className="rutas">CARTAS</span>
+            <h3 className="mb-0">
+              <b>CARTAS</b>
+            </h3>
+            <span>Cartas disponibles en la plataforma</span>
+          </div>
+          <div className="text-right col-2">
+            <button type="button" className="btn btn-outline-success mt-3 mr-2">
+              <IoMdAdd className="icoAdd" />{" "}
+              <span className="btnColor">Nuevo</span>
+            </button>
+          </div>
+        </div>
+        
+        {/* CUERPO DE LA TABLA */}
+        <div className="row bodyTable">
+          <div className="col-12">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Imagen</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Tipo de Carta</th>
+                  <th scope="col">Daño</th>
+                  <th scope="col">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row"></th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </Layout>
-    )
+    );
   }
 }
+
+
 
 export default Home;
