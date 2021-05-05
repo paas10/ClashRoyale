710457f5cda1c0fe74503c0c97bc056efd59a47c
Pablo Alvarado <pablo.andre_alvarado@hotmail.com>
2021-05-04 22:18:38 -0600

    [Created]: Error handling of creation user requests, notifications and Redirect to Login

--- clashroyale/src/components/SignUp.js
+++ clashroyale/src/components/SignUp.js
@@ -1,21 +1,46 @@
 import React, { useState } from "react";
 import corona from "./../images/corona.png";
 import UserPool from '../UserPool';
+import { useHistory } from "react-router-dom";
+import { store } from 'react-notifications-component';
 import 'crypto-js/lib-typedarrays';
 
-export default () => {
+const SignUp = () => {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  var emailError, passwordError;
+  var emailError, passwordError, loginError;
   
   const onSubmit = event => {
     event.preventDefault();
 
     if (!validateErrors(email, password)) return;
 
-    UserPool.signUp(email, password, [], null, (err, data) => {
-      if (err) console.error(err);
-      console.log(data)
+    UserPool.signUp(email, password, [], null, (res, data) => {
+      if (data) {
+        console.log('DATA ', data)
+
+        store.addNotification({
+          message: "Registro Exitoso! Confirma tu dirección de correo electrónico para continuar.",
+          type: "success",
+          insert: "top",
+          container: "top-right",
+          animationIn: ["animate__animated", "animate__fadeIn"],
+          animationOut: ["animate__animated", "animate__fadeOut"],
+          dismiss: {
+            duration: 5000,
+            showIcon: true,
+            onScreen: true
+          }
+        });
+
+        history.push("/login");
+      }
+
+      if (res.code && res.code.includes('Exception')){
+        console.log('res ', res)
+        manageErrors(res.message)
+      } 
     });
   }
 
@@ -44,6 +69,51 @@ export default () => {
     }
   
     return true;
+  }
+
+  function hideErrors(param) {
+    document.getElementById('loginError').hidden = true
+
+    if(param === 'pass')
+      document.getElementById('passwordError').hidden = true
+    if(param === 'email')
+      document.getElementById('emailError').hidden = true
+  }
+
+  function manageErrors(message) {
+    loginError = document.getElementById('loginError');
+    emailError = document.getElementById('emailError');
+    passwordError = document.getElementById('passwordError');
+    loginError.hidden = true;
+    emailError.hidden = true;
+    passwordError.hidden = true;
+
+    if(message === "1 validation error detected: Value at 'password' failed to satisfy constraint: Member must have length greater than or equal to 6" || message === "Password did not conform with policy: Password not long enough") {
+      passwordError.innerHTML = "La contraseña debe tener al menos 8 caracteres."
+      passwordError.hidden = false;
+    } else if(message === "Password did not conform with policy: Password must have numeric characters") {
+      passwordError.innerHTML = "La contraseña debe contener números."
+      passwordError.hidden = false;
+    } else if(message === "Password did not conform with policy: Password must have uppercase characters") {
+      passwordError.innerHTML = "La contraseña debe contener letras mayúsculas."
+      passwordError.hidden = false;
+    } else if(message === "Password did not conform with policy: Password must have lowercase characters") {
+      passwordError.innerHTML = "La contraseña debe contener letras minúsculas."
+      passwordError.hidden = false;
+    } else if(message === "Password did not conform with policy: Password must have symbol characters") {
+      passwordError.innerHTML = "La contraseña debe contener símbolos."
+      passwordError.hidden = false;
+    } else if (message === "Username should be an email.") {
+      emailError.innerHTML = "El correo electrónico no es válido."
+      emailError.hidden = false;
+    } else if (message === "An account with the given email already exists.") {
+      emailError.innerHTML = "El correo electrónico ingresado no se encuentra disponible."
+      emailError.hidden = false;
+    } else {
+      loginError.innerHTML = message
+      loginError.hidden = false;
+    }
+    
   }
 
   return (
@@ -75,7 +145,7 @@ export default () => {
             </div>
             <div className="row mt-1">
               <div className="col-12">
-                <input className="form-control" type="text" value={email} onChange={(event) => {setEmail(event.target.value); document.getElementById('emailError').hidden = true}} />
+                <input className="form-control" type="text" value={email} onChange={(event) => {setEmail(event.target.value); hideErrors('email')}} />
                 <span className="error" id="emailError" hidden></span>
               </div>
             </div>
@@ -86,14 +156,14 @@ export default () => {
             </div>
             <div className="row mt-1">
               <div className="col-12">
-                <input className="form-control" type="password" value={password} onChange={ (event) => {setPassword(event.target.value); document.getElementById('passwordError').hidden = true}} />
+                <input className="form-control" type="password" value={password} onChange={ (event) => {setPassword(event.target.value); hideErrors('pass')}} />
                 <span className="error" id="passwordError" hidden></span>
               </div>
             </div>
             <div className="row mt-2">
               <div className="col-12">
                 <button type='submit' className="btn btn-block btnlogin" id="btnlogin" style={({ marginTop: '40px' })} >Crear Cuenta</button>
-                <span className="error" id="loginError" hidden></span>
+                <span className="error" id="loginError" hidden style={( { marginTop: '5px' } )}></span>
               </div>
             </div>
           </form>
@@ -102,5 +172,6 @@ export default () => {
       </div>
     </div>
   );
-
 }
+
+export default SignUp;
